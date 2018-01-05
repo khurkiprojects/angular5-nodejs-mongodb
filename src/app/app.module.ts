8309b6ccd03a991ee975e264e1c6791bfba3c169
khurkiprojects <susmit.projects@gmail.com>
2018-01-05 22:57:43 +0530

    Updated html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-
+ 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
